3a214cc53069b6bc010f4b71a63b74d87f118928
z50205 <z50205@yahoo.com.tw>
2023-12-26 01:04:26 +0800

    0.2
    Signed-off-by: Biz <00000000@yahoo.com.tw>

---
+++ twittor/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,BooleanField,SubmitField
+from wtforms.validators import DataRequired
+
+class Loginform(FlaskForm):
+    username=StringField("username",validators=[DataRequired()])
+    password=PasswordField("Password",validators=[DataRequired()])
+    remember_me=BooleanField("Remember me")
+    submit =SubmitField('SignIn')
