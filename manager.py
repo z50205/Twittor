71ece4b708fc695f704c9fdff6151b22b39ff66c
Biz <00000000@yahoo.com.tw>
2023-12-31 01:41:47 +0800

    Version 0.3
    Sined-off-by: Biz <00000000@yahoo.com.tw>

--- manager.py
+++ manager.py
@@ -1,9 +1,18 @@
 from flask import Flask
-from flask.cli import AppGroup
-from twittor import create_app
+from flask.cli import FlaskGroup
+from flask_migrate import Migrate
+from twittor import create_app, db
+from twittor.models import User
 
-app=create_app()
-manager = AppGroup(app)
+
+app = create_app()
+migrate = Migrate(app, db)
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(app=app, db=db, User=User)
+
+cli=FlaskGroup(app)
 
 if __name__ == "__main__":
-    app.run()
+    cli()
