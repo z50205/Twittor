274d7e75b94eb3a3ef221632181aea59e819eb8c
Biz <00000000@yahoo.com.tw>
2024-01-02 23:23:22 +0800

    Version 0.4

--- twittor/config.py
+++ twittor/config.py
@@ -5,3 +5,4 @@ config_path=os.path.abspath(os.path.dirname(__file__))
 
 class Config:    
     SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URL","sqlite:///"+os.path.join(config_path,"twittor.db"))
+    SECRET_KEY='abc123'
